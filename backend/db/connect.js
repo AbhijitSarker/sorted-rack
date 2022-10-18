bbdf9a548d33ff970cf1bcabcfa1f6821a0150d8
Daisyn <daisyn@sjinnovation.com>
2022-10-18 18:30:49 +0530

    cleared master

--- backend/db/connect.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-
-const connDb = (url) =>{
-    return mongoose.connect(url);
-};
-
-module.exports = connDb;
--- backend/utility/index.js
+++
@@ -1,8 +0,0 @@
-const checkPermission = require('./checkPermission')
-const checkUserRole = require('./checkUserRole')
-
-
-module.exports={
-    checkPermission,
-    checkUserRole
-}
